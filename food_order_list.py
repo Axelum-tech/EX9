934e0d01724692585aad766aad0ab2a1ed031fe7
Axelum-tech <74005144+Axelum-tech@users.noreply.github.com>
2021-01-12 15:40:20 +0200

    Add files via upload

--- food_order_list.py
+++ food_order_list.py
@@ -7,9 +7,17 @@ order_food_q     = [     2,       1 ,      2] #list> int
 for i in range(len(order_food_price)):  
     print(i+1,order_food_names[i], order_food_price[i],"u.c", order_food_q[i])
     
-    if i==2: 
-        print("TOTAL=",order_food_price[0]*order_food_q[0]+order_food_price[1]*order_food_q[1]+order_food_price[2]*order_food_q[2] )
-        print(order_food_price[i]*order_food_q[i])
+
+for i in range(len(order_food_price)):
+    TOTAL=order_food_price[i]*order_food_q[i]
+    print("TOTAL=",TOTAL)
+
+
+
+
+    # if i==2: 
+    #     print("TOTAL=",order_food_price[0]*order_food_q[0]+order_food_price[1]*order_food_q[1]+order_food_price[2]*order_food_q[2] )
+    #     print(order_food_price[i]*order_food_q[i])
 
 
     
